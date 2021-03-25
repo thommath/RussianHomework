9177c58ee30a7552dd8663d573e0fbee5be6e79a
Thommath <thomlmath@gmail.com>
2021-03-25 08:38:12 -0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,109 +1,255 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <button @click="reset">To the top</button>
+
+    <h1 v-if="element.childHeader">{{element.childHeader}}</h1>
+    <h2 v-if="element.hint">{{element.hint}}</h2>
+
+    <h1 v-if="element.result">{{element.result}}</h1>
+
+    <button v-for="b in element.children" :key="b.text" @click="navigate(b)" >{{b.text}}</button>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 
+type Alternative = {
+  text: string;
+  childHeader?: string;
+  children?: Alternative[];
+  result?: string;
+  hint?: string;
+}
+
+const root: Alternative = {
+    text: "Root",
+    childHeader: "What do you want to conjugate?",
+    children: [
+    {
+      text: "Noun",
+      childHeader: "What is the ending?",
+      children: [
+        {
+          text: "Consonant",
+          result: "Masculine",
+        },
+        {
+          text: "й",
+          result: "Masculine",
+        },
+        {
+          text: "ь",
+          result: "Masculine",
+        },
+        {
+          text: "a",
+          result: "Feminine",
+        },
+        {
+          text: "я",
+          result: "Feminine",
+        },
+        {
+          text: "ия",
+          result: "Feminine",
+        },
+        {
+          text: "ь",
+          result: "Feminine",
+        },
+        {
+          text: "o",
+          result: "Neuter",
+        },
+        {
+          text: "e",
+          result: "Neuter",
+        },
+        {
+          text: "иe",
+          result: "Neuter",
+        },
+      ],
+    },
+    {
+      text: "Nomiative plural",
+      hint: "The stress in some regular nouns change in the nominative plural. This can happen for all genders",
+      childHeader: "What is the gender?",
+      children: [
+        {
+          text: "Masculine",
+          childHeader: "What is the ending?",
+          children: [
+            {
+              text: "consonant",
+              result: "Add ы",
+            },
+            {
+              text: "й",
+              result: "Remove й, add и",
+            },
+            {
+              text: "ь",
+              result: "Remove ь, add и",
+            },
+          ]
+        },
+        {
+          text: "Feminine",
+          childHeader: "What is the ending?",
+          children: [
+            {
+              text: "a",
+              result: "Remove a, add ы",
+            },
+            {
+              text: "я",
+              result: "Remove я, add и",
+            },
+            {
+              text: "ь",
+              result: "Remove ь, add и",
+            },
+          ]
+        },
+        {
+          text: "Neuter",
+          childHeader: "What is the ending?",
+          children: [
+            {
+              text: "o",
+              result: "Remove o, add a",
+            },
+            {
+              text: "e",
+              result: "Remove e, add я",
+            },
+            {
+              text: "иe",
+              result: "It's already perfect",
+            },
+          ]
+        },
+      ],
+    },
+    {
+      text: "Accusative singular",
+      hint: "In the singular masculinne nouns only change in the accusative case if they are animate",
+      childHeader: "What is the gender?",
+      children: [
+        {
+          text: "Masculine",
+          childHeader: "What is the ending?",
+          hint: "In the singular masculinne nouns only change in the accusative case if they are animate",
+          children: [
+            {
+              text: "consonant",
+              result: "Add a",
+            },
+            {
+              text: "й",
+              result: "Remove й, add я",
+            },
+            {
+              text: "ь",
+              result: "Remove ь, add я",
+            },
+          ]
+        },
+        {
+          text: "Feminine",
+          childHeader: "What is the ending?",
+          children: [
+            {
+              text: "a",
+              result: "Remove a, add у",
+            },
+            {
+              text: "я",
+              result: "Remove я, add ю",
+            },
+            {
+              text: "ь",
+              result: "Same same",
+            },
+          ]
+        },
+        {
+          text: "Neuter",
+          result: "They are inanimate and do not change (with some very rare exceptions)"
+        },
+      ],
+    },
+
+    {
+      text: "Accusative plural",
+      hint: "In the plural inanimate nouns are the same as nominative plural - maybe go there?",
+      childHeader: "What is the gender?",
+      children: [
+        {
+          text: "Masculine",
+          childHeader: "What is the ending?",
+          hint: "In the singular masculinne nouns only change in the accusative case if they are animate",
+          children: [
+            {
+              text: "consonant",
+              result: "Add oв",
+            },
+            {
+              text: "й",
+              result: "Remove й, add eв",
+            },
+            {
+              text: "ь",
+              result: "Remove ь, add eй",
+            },
+          ]
+        },
+        {
+          text: "Feminine",
+          childHeader: "What is the ending?",
+          children: [
+            {
+              text: "a",
+              result: "Remove a",
+            },
+            {
+              text: "я",
+              result: "Remove я, add ь",
+            },
+            {
+              text: "ь",
+              result: "Remove ь, add eй",
+            },
+          ]
+        },
+        {
+          text: "Neuter",
+          result: "They are inanimate and do not change (with some very rare exceptions)"
+        },
+      ],
+    }
+  ]
+  };
+
 @Options({
-  props: {
-    msg: String,
-  },
 })
 export default class HelloWorld extends Vue {
-  msg!: string;
+
+  curElem: Alternative = root;
+
+  get element() {
+    return this.curElem;
+  }
+
+  reset() {
+    this.curElem = root;
+  }
+  navigate(b: Alternative) {
+    this.curElem = b;
+  }
 }
 </script>
 
@@ -122,5 +268,11 @@ li {
 }
 a {
   color: #42b983;
+}
+
+button {
+  padding: 0.3em;
+  margin: 0.3em;
+  cursor: pointer;
 }
 </style>
